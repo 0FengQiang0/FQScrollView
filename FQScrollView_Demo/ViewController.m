94b886f5f176d4351525295878e41b12d9efc8e8
fengqiang <fengqiang@many-it.com>
2017-07-24 15:16:17 +0800

    修改了网络图片加载的问题

--- FQScrollView_Demo/ViewController.m
+++ FQScrollView_Demo/ViewController.m
@@ -25,7 +25,7 @@
     [self.view addSubview:scrollView];
     
     // 加载图片资源,可以是本地的,也可以是URL ...
-    scrollView.imageArray = @[@"01.jpg",@"02.jpg",@"http://avatar.csdn.net/6/3/A/1_qq_32497905.jpg"];
+    scrollView.imageArray = @[@"01.jpg",@"02.jpg",@"http://tva3.sinaimg.cn/crop.147.174.217.217.180/006prxrdjw8f2ywwpdmplj30aw0aw3yw.jpg"];
     
     // 因为数组里有以 http 开头的url，需要去 info.plist文件里做一下配置...否则无法显示网络图片 ...  加上  App Transport Security Settings
 }
